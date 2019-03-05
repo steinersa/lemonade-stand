382430ee232414a5208b91c8072ffc92c682baa6
Sarah Steiner <steiner.sa96@gmail.com>
2019-03-05 14:58:54 -0600

    Store is complete. Next step is to work on if player wants to change recipe.

--- LemonadeStand/Store.cs
+++ LemonadeStand/Store.cs
@@ -8,12 +8,21 @@ namespace LemonadeStand
     {
         //member variables
         public string storeChoice;
+
         public string lemonsPurchased;
         public double priceOfFiveLemons;
         public double priceOfTenLemons;
         public double priceOfFifteenLemons;
 
+        public string sugarPurchased;
+        public double priceOfFiveSugar;
+        public double priceOfTenSugar;
+        public double priceOfFifteenSugar;
 
+        public string icePurchased;
+        public double priceOfFiveIce;
+        public double priceOfTenIce;
+        public double priceOfFifteenIce;
 
         //constructor
         public Store()
@@ -21,12 +30,20 @@ namespace LemonadeStand
             this.priceOfFiveLemons = 3.00;
             this.priceOfTenLemons = 5.00;
             this.priceOfFifteenLemons = 7.00;
+
+            this.priceOfFiveSugar = 5.00;
+            this.priceOfTenSugar = 9.00;
+            this.priceOfFifteenSugar = 13.00;
+
+            this.priceOfFiveIce = 2.00;
+            this.priceOfTenIce = 4.00;
+            this.priceOfFifteenIce = 6.00;
         }
 
         //member methods
         public void SellToPlayer(Player player, Inventory inventory, Store store)
         {
-            Console.WriteLine("Would you like to buy 'lemons', 'sugar', or 'ice'? If you're all done shopping, please enter 'leave store'.");
+            Console.WriteLine("Would you like to buy 'lemons', 'sugar', or 'ice'? If you're all done shopping, please type 'leave store'.");
             storeChoice = Console.ReadLine();
 
             switch (storeChoice)
@@ -36,10 +53,12 @@ namespace LemonadeStand
                     SellToPlayer(player, inventory, store);
                     break;
                 case "sugar":
-                    SellSugar();
+                    SellSugar(player, inventory);
+                    SellToPlayer(player, inventory, store);
                     break;
                 case "ice":
-                    SellIce();
+                    SellIce(player, inventory);
+                    SellToPlayer(player, inventory, store);
                     break;
                 case "leave store":
                     player.ChooseWhatToDoAtStartOfDay(inventory, store, player);
@@ -82,14 +101,62 @@ namespace LemonadeStand
 
         }
 
-        public void SellSugar()
+        public void SellSugar(Player player, Inventory inventory)
         {
-
+            Console.WriteLine("Would you like to buy 'five' packs of sugar for $5.00, 'ten' for $9.00, or 'fifteen' for $13.00?");
+            sugarPurchased = Console.ReadLine();
+            if (sugarPurchased == "five")
+            {
+                player.cash -= priceOfFiveSugar;
+                inventory.sugarInventory = +5;
+                Console.WriteLine("Thanks for your purchase! You now have {0} dollars.", player.cash);
+            }
+            else if (sugarPurchased == "ten")
+            {
+                player.cash -= priceOfTenSugar;
+                inventory.sugarInventory = +10;
+                Console.WriteLine("Thanks for your purchase! You now have {0} dollars.", player.cash);
+            }
+            else if (sugarPurchased == "fifteen")
+            {
+                player.cash -= priceOfFifteenSugar;
+                inventory.sugarInventory = +15;
+                Console.WriteLine("Thanks for your purchase! You now have {0} dollars.", player.cash);
+            }
+            else
+            {
+                Console.WriteLine("Not an option. Try again!");
+                SellSugar(player, inventory);
+            }
         }
 
-        public void SellIce()
+        public void SellIce(Player player, Inventory inventory)
         {
-
+            Console.WriteLine("Would you like to buy 'five' bundles of ice for $2.00, 'ten' for $4.00, or 'fifteen' for $6.00?");
+            icePurchased = Console.ReadLine();
+            if (icePurchased == "five")
+            {
+                player.cash -= priceOfFiveIce;
+                inventory.iceInventory = +5;
+                Console.WriteLine("Thanks for your purchase! You now have {0} dollars.", player.cash);
+            }
+            else if (icePurchased == "ten")
+            {
+                player.cash -= priceOfTenIce;
+                inventory.iceInventory = +10;
+                Console.WriteLine("Thanks for your purchase! You now have {0} dollars.", player.cash);
+            }
+            else if (icePurchased == "fifteen")
+            {
+                player.cash -= priceOfFifteenIce;
+                inventory.iceInventory = +15;
+                Console.WriteLine("Thanks for your purchase! You now have {0} dollars.", player.cash);
+            }
+            else
+            {
+                Console.WriteLine("Not an option. Try again!");
+                SellIce(player, inventory);
+            }
         }
 
     }
